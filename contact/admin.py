118bbbabbc6194c9f889fec8492ee746f0acd4f5
Mouris <mourisworks@hotmail.com>
2026-07-27 14:23:44 -0300

    Adding admin panels.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'phone', 'email',
+    ordering = '-id',
+
+    search_fields = 'id' ,'first_name', 
+    list_display_links = 'id', 'first_name',
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('contacts/', include('contact.urls')),
-    path('admin/', admin.site.urls),
+    path('contact/', include('contact.urls')),
+    path('contact/admin/', admin.site.urls),
 ]
