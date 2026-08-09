8a41c032d935418beb24d23ea53105881e49a674
Mouris <mourisworks@hotmail.com>
2026-08-09 15:42:26 -0300

    Form apply

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,8 @@
-from django.http import Http404
+from django import http
+from django.db.models import Q
+from django.http import Http404, HttpRequest, request
 from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
-
 # Create your views here.
 
 def index(request):
@@ -22,7 +23,6 @@ def index(request):
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
-    
     context = {
         'contact': single_contact,
     }
@@ -31,3 +31,28 @@ def contact(request, contact_id):
         'contact/contact.html',
         context,
         )
+
+def search(request: HttpRequest):
+    search_value = request.GET.get('q','')
+
+    contacts = Contact.objects\
+            .filter(show=True)\
+            .filter(
+                Q(first_name__icontains=search_value) |
+                Q(last_name__icontains=search_value)  |
+                Q(phone__icontains=search_value)  |
+                Q(email__icontains=search_value)
+            )\
+            .order_by('-id')[10:20] 
+
+
+    context = {
+        "contacts": contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
