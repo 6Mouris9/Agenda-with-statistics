fe05c0b64df2c177590b787cac0f21c6a99d6acc
Mouris <mourisworks@hotmail.com>
2026-08-24 11:16:11 -0300

    search fix

--- contact/urls.py
+++ contact/urls.py
@@ -1,13 +1,13 @@
-
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
-    path('', views.index, name='index'),  # type:ignore
+    path('search/', views.search, name='search'),
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('', views.index, name='index'),
 ]
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,59 +1,55 @@
-from django import http
 from django.db.models import Q
-from django.http import Http404, HttpRequest, request
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, render
+
 from contact.models import Contact
-# Create your views here.
+
 
 def index(request):
-    contacts = Contact.objects\
-    .filter(show=True)\
-    .order_by('-id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
     context = {
-        "contacts": contacts,
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
-    return render(
-        request,
-        'contact/index.html',
-        context=context
-    )
+    return render(request, 'contact/index.html', context=context)
+
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
     context = {
         'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} - ',
     }
-    return render(
-        request,
-        'contact/contact.html',
-        context,
-        )
+
+    return render(request, 'contact/contact.html', context)
+
 
 def search(request: HttpRequest):
-    search_value = request.GET.get('q','').strip()
+    search_value = request.GET.get('q', '').strip()
 
-    contacts = Contact.objects\
-            .filter(show=True)\
-            .filter(
-                Q(first_name__icontains=search_value) |
-                Q(last_name__icontains=search_value)  |
-                Q(phone__icontains=search_value)  |
-                Q(email__icontains=search_value)
-            )\
-            .order_by('-id')[10:20] 
+    contacts = Contact.objects.filter(show=True)
 
+    if search_value:
+        search_terms = search_value.split()
+        query = Q()
+
+        for term in search_terms:
+            query |= Q(first_name__icontains=term)
+            query |= Q(last_name__icontains=term)
+            query |= Q(phone__icontains=term)
+            query |= Q(email__icontains=term)
+
+        contacts = contacts.filter(query)
+
+    contacts = contacts.order_by('-id')[:10]
 
     context = {
-        "contacts": contacts,
-        'site_title': 'Contatos - ',
-        "search_value": search_value,
+        'contacts': contacts,
+        'site_title': 'Busca - ',
+        'search_value': search_value,
     }
 
-    return render(
-        request,
-        'contact/index.html',
-        context,
-    )
+    return render(request, 'contact/index.html', context)
